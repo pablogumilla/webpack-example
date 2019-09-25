8cd1b0277642286d4b0907bab9e1a79f456d24f7
Pablo Gumilla <pgumilla@eventbrite.com>
2019-09-25 10:29:57 -0300

    Use manual code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 
 module.exports = {
+  entry: {
+    index: './src/index.js',
+    header: './src/components/Header.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
